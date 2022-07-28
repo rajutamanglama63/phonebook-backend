cb7ba003bdb3b8f94bdde9e9db467c42f4f0482a
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-28 21:00:55 +0545

    uibuild

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ let persons = [
 ]
 
 // checking routes
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>")
-})
+// app.get("/", (req, res) => {
+//     res.send("<h1>Hello World</h1>")
+// })
 
 // api to get lists of person
 app.get("/api/persons", (req, res) => {
