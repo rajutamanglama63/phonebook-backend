7398b979c5c47da7e67ba8b3a63ab7faa862bc64
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-31 15:04:19 +0545

    date : date functionality added

--- server.js
+++ server.js
@@ -63,16 +63,15 @@ app.get("/api/persons", (req, res, next) => {
 
 // api to get phonebook info
 app.get("/api/info", (req, res) => {
-  const date = new Date;
+  const date = new Date();
 
   Contact.find().then((allContact) => {
     res
-    .status(200)
-    .send(
-      `<p>Phonebook has info for ${allContact.length} people</p><p>${date}</p>`
-    );
-  })
- 
+      .status(200)
+      .send(
+        `<p>Phonebook has info for ${allContact.length} people</p><p>${date}</p>`
+      );
+  });
 });
 
 // api to update specific person's contact info
@@ -84,9 +83,19 @@ app.put("/api/persons/:id", (req, res, next) => {
     number: req.body.number,
   };
 
-  Contact.findByIdAndUpdate(contactId, contactToUpdate, { new: true, runValidators : true, context : "query" })
+  Contact.findByIdAndUpdate(contactId, contactToUpdate, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedContact) => {
-      res.status(200).json({success : true, msg : "Contact updated successfully.", updatedContact});
+      res
+        .status(200)
+        .json({
+          success: true,
+          msg: "Contact updated successfully.",
+          updatedContact,
+        });
     })
     .catch((err) => next(err));
 });
@@ -162,10 +171,19 @@ app.post("/api/persons/", (req, res, next) => {
     date: new Date(),
   });
 
-  contact.save().then((savedContact) => {
-    res.status(200).json({success : true, msg : "Contact successfully created.", savedContact});
-  }).catch((err) => next(err));
-})
+  contact
+    .save()
+    .then((savedContact) => {
+      res
+        .status(200)
+        .json({
+          success: true,
+          msg: "Contact successfully created.",
+          savedContact,
+        });
+    })
+    .catch((err) => next(err));
+});
 
 // ERROR HANDLER MIDDLEWARE
 const errorHandler = (err, req, res, next) => {
@@ -173,8 +191,8 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.name === "CastError") {
     return res.status(400).send({ err: "malformatted id" });
-  } else if(err.name === "ValidationError") {
-    return res.status(400).json({err : err.message})
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ err: err.message });
   }
 
   next(err);
