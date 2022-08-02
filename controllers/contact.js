808b41f9b1c3fac92311fd1c3a52118635a5da56
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-02 15:03:17 +0545

    config: route of contactSchema configured

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Contact = require("./models/contactSchema");
+const Contact = require("../models/contactSchema");
 
 const contactRouter = express.Router();
 
